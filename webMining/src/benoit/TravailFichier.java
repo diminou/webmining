22933cf58c958544000047922a11c2fc0014495e
benoit <benoit@benoit-HP-ZBook-15>
2014-12-19 12:35:08 +0100

    propre

---
+++ webMining/src/benoit/TravailFichier.java
@@ -0,0 +1,29 @@
+package benoit;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class TravailFichier {
+	
+	/**
+	 * Retourne une HashMap comprenant
+	 * @param listeMap
+	 * @return
+	 */
+	public static HashMap<String,Integer> RegrouperHashMaps(List<HashMap<String,Integer>> listeMap){
+		
+		HashMap<String,Integer> mapComplete=new HashMap<String,Integer>();
+		
+		
+		
+		return mapComplete;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
