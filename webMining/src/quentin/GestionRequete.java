02923842bacfc5993653f81cf81808a26e9509fa
Nollain <quentin.nollet@hotmail.fr>
2014-12-27 15:16:15 +0100

    Méthode retournant les digrammes d'une requête à partir de la liste contennat les mots de la requête implémentée.

--- webMining/src/quentin/GestionRequete.java
+++ webMining/src/quentin/GestionRequete.java
@@ -8,6 +8,8 @@ import org.annolab.tt4j.TokenHandler;
 import org.annolab.tt4j.TreeTaggerWrapper;
 import org.tartarus.snowball.SnowballStemmer;
 
+import tools.FrenchStemmer;
+
 
 
 public class GestionRequete {
@@ -37,26 +39,22 @@ public class GestionRequete {
 	public List<String> requeteLemm(List<String> requeteSplit){
 		List<String> listeLemm = new ArrayList<String>();
 		//TODO
+
 		
-		
-		return listeLemm;	
-	}
-	
-	
-	
-	/**
-	 * 
-	 * @param requeteSplit : Liste des mots de la requete de l'utilisateur
-	 * @return la liste stematisée de requeteSplit
-	 */
-	public List<String> requeteStem(List<String> requeteSplit){
-		List<String> listeStem = new ArrayList<String>();
-		//TODO
-	
 		TreeTaggerWrapper<String> tt = new TreeTaggerWrapper<String>();
-		String res = "";
+//		String res = "";
 		try {
 			tt.setModel("fichierPartage/french-utf8.par");
+//			tt.s
+			tt.setHandler(new TokenHandler<String>() {
+				String res;
+				@Override
+				public void token(String token, String pos, String lemma) {
+					// TODO Auto-generated method stub
+					res=lemma;
+//					listeStem.add(lemma);
+				}
+			});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -82,11 +80,25 @@ public class GestionRequete {
 			
 //			listeStem.add();
 		}
-		return listeStem;	
+		
+		return listeLemm;	
 	}
 	
 	
 	
+	/**
+	 * 
+	 * @param requete: Mots de la requete de l'utilisateur
+	 * @return la liste stematisée de requeteSplit
+	 */
+	public List<String> requeteStem(String requete){
+//		List<String> listeStem = new ArrayList<String>();
+		//TODO
+	
+		ArrayList<String> words = (new FrenchStemmer()).normalize(requete);
+		
+		return words;	
+	}
 	
 	
 	
@@ -95,13 +107,14 @@ public class GestionRequete {
 	/**
 	 * 
 	 * @param listeMot : Liste des mots 
-	 * @return la liste des digramme provenant de listeMot
+	 * @return la liste des digramme provenant de listeMot (sous la forme [mot1Digramme mot2Digramme])
 	 */
 	
 	public List<String> listeDigramme(List<String> listeMot){
 		List<String> listeDigramme = new ArrayList<String>();
-		//TODO
-		
+		for(int i = 0; i< listeMot.size()-1; i++){
+			listeDigramme.add(listeMot.get(i) + " " +  listeMot.get(i+1));
+		}
 		return listeDigramme; 
 	}
 	
@@ -114,6 +127,13 @@ public class GestionRequete {
 	public List<String> listeDisciminante(List<String> listeMot){
 		List<String> listeDisc = new ArrayList<String>();
 		//TODO
+		
+		for(String m : listeMot){
+
+//			if( m.isutile){
+//				listeDisc.add(m);
+//			}
+		}
 		
 		return(listeDisc);
 	}
